f4a9ea91ebb59a0ecd9869ba4a366994c28e2e73
Shyrokopoyas Andrew <fixerrisen@gmail.com>
2018-07-03 23:54:33 +0300

    Set common requests

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,10 @@ const setEmployeesRequests = require('./employees');
 const setProjectsRequests = require('./projects');
 const setAuthRequests = require('./auth');
 const setTasksRequests = require('./tasks');
+const setCommonRequests = require('./common');
 
 module.exports = app => {
+  setCommonRequests(app);
   setAuthRequests(app);
   setTasksRequests(app);
   setEmployeesRequests(app);
